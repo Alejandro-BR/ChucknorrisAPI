cc69a270906d50722245c606144ccfc6d3822d52
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-09-27 17:37:49 +0200

    Inicial

---
+++ src/app/pagas/chucknorris-random/chucknorris-random.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chucknorris-random',
+  standalone: true,
+  imports: [],
+  templateUrl: './chucknorris-random.component.html',
+  styleUrl: './chucknorris-random.component.css'
+})
+export class ChucknorrisRandomComponent {
+
+}
